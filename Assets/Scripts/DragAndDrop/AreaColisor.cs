d18732d06c7e1affdd28f536644daeb1edaced30
Ricardo Cavalcante <ricardocavalcantesilva@gmail.com>
2018-02-20 22:06:53 -0300

    Drag and drop
    Ajustes

---
+++ Assets/Scripts/DragAndDrop/AreaColisor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaColisor : MonoBehaviour {
+
+    void Update () {
+        Collider2D myCollider = gameObject.GetComponent<Collider2D>();
+
+        //Quantidade máxima de colisores que podem colidir com o objeto
+        int numColliders = 10;
+
+        Collider2D[] colliders = new Collider2D[numColliders];
+        ContactFilter2D contactFilter = new ContactFilter2D();
+
+        int colliderCount = myCollider.OverlapCollider(contactFilter, colliders);
+        Debug.Log(colliderCount);
+    }
+}
