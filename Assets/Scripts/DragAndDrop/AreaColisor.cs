ae5f23faa388c0efa42bd27874491c6445bf23ec
Unknown <diegolol@hotmail.com>
2018-02-21 23:43:55 -0300

    Draaaaaaaaaag

--- Assets/Scripts/DragAndDrop/AreaColisor.cs
+++ Assets/Scripts/DragAndDrop/AreaColisor.cs
@@ -2,23 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TouchScript.Gestures;
+using TouchScript.Gestures.TransformGestures;
+using TouchScript.Behaviors;
 
-public class AreaColisor : MonoBehaviour {
+public class AreaColisor : MonoBehaviour
+{
+    Transform _transform;
 
     Collider2D myCollider;
     int numColliders = 1;
 
+    bool _beingHeld = false;
+
+
+    PressGesture _press;
+    ReleaseGesture _release;
+
     private void Awake()
     {
+        _transform = GetComponent<Transform>();
         myCollider = gameObject.GetComponent<Collider2D>();
+
+        _press = GetComponent<PressGesture>();
+        _press.OnPress.AddListener(StartDrag);
+
+        _release = GetComponent<ReleaseGesture>();
+        _release.OnRelease.AddListener(StopDrag);
     }
 
-    void Update() {
+    void StartDrag(Gesture gesture)
+    {
+        Debug.Log("Drag Start");
+        _beingHeld = true;
+    }
+
+    void StopDrag(Gesture gesture)
+    {
+        _beingHeld = false;
+
         Collider2D[] colliders = new Collider2D[numColliders];
         ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(1 << LayerMask.NameToLayer("DropArea"));
 
         int colliderCount = myCollider.OverlapCollider(contactFilter, colliders);
         Debug.Log(colliderCount);
     }
 
+    private void Update()
+    {
+        if(_beingHeld)
+        {
+            Vector2 mousePos = Input.mousePosition;
+
+            _transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+    }
 }
