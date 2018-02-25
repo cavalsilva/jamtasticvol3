1a8fab4986284cfbff0273a68a6eacea31a18e0c
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-02-25 07:11:51 -0300

    Creditos, SFX
    Criação da tela de Créditos e SFXController.

---
+++ Assets/Scripts/Music/SFXController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour {
+
+    public AudioClip audioClipClick;
+    public AudioClip audioClipDrag;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySoundClick()
+    {
+        Debug.Log("Tocou Som Click");
+        audioSource.clip = audioClipClick;
+        audioSource.Play();
+    }
+
+    public void PlaySoundDrag()
+    {
+        Debug.Log("Tocou Som Drag");
+        audioSource.clip = audioClipDrag;
+        audioSource.Play();
+    }
+}
