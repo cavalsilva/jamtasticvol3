b7b8bab86d3b32ccba5c79b3fce00796003b3526
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-02-24 22:32:27 -0300

    Music Controller

---
+++ Assets/Scripts/Music/MusicController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+    public AudioClip[] audioClip;
+    private AudioSource audioSource;
+    private int indexAudio = 0;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        //Verifica se existe algum duplicado, se sim então exclui
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioClip.Length > 0)
+            PlayNextClip();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            indexAudio += 1;
+            PlayNextClip();
+        }
+    }
+
+    private void PlayNextClip()
+    {
+        if (indexAudio == audioClip.Length)
+            indexAudio = 0;
+
+        Debug.Log("Tocando música do index: " + indexAudio);
+        audioSource.PlayOneShot(audioClip[indexAudio]);
+    }
+}
